3f07af398668276255e729bb2603a5350745b692
Hpyer <me@hpyer.cn>
2022-04-25 21:15:21 +0800

    fix: 调整微信支付的证书配置说明

--- config/Payment.js
+++ config/Payment.js
@@ -8,6 +8,6 @@ module.exports = {
   key: 'key-for-signature',
   // 默认回调地址，也可以在下单时单独设置来覆盖它，完整URL，不带参数
   notify_url: 'http://xxx.com/pay/notify',
-  // 证书地址，Node.js 只需要 .pfx 证书文件即可
-  cert_path: 'path/to/your/cert.pfx', // XXX: 绝对路径！！！！
+  // 证书地址，Node.js 只需要 apiclient_cert.p12 证书文件即可
+  cert_path: 'path/to/your/apiclient_cert.p12', // XXX: 绝对路径！！！！
 };
